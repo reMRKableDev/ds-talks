b8f17447269deac6aed9305b52c9e3f895acb8d5
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-01-22 12:08:21 +0100

    :art: Exchanged react-router for reach router

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <Router>
+      <section className="App">
+        <Switch>
+
+        </Switch>
+        <HomePage />
+      </section>
+    </Router>
+  );
+}
+
+export default App;
