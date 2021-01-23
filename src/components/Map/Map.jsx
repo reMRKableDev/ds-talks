068f2f260465becb7593030116233bb7aeb4c229
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-01-23 22:18:00 +0100

    :fire: Deleted comments

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,7 +6,6 @@ import Layer from '../Layer/Layer';
 import MarkerIcon from '../MarkerIcon/MarkerIcon';
 
 const Map = () => {
-  // const mapReference = useRef();
   const CENTER = { lat: 52.377956, lng: 4.89707 };
   const ZOOM_LEVEL = 3;
   const MAX_ZOOM = 18;
@@ -14,7 +13,7 @@ const Map = () => {
   return (
     <section>
       <h2>See map for all episodes</h2>
-      
+
       <MapContainer center={CENTER} zoom={ZOOM_LEVEL} maxZoom={MAX_ZOOM}>
         <Layer />
         <MarkerIcon />
