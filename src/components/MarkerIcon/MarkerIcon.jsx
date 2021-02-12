b7d01ac36ad632fd9a526bec29cb2b9a3c31df00
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-02-12 19:06:08 +0100

    :recycle: refactoring icon

--- src/components/MarkerIcon/MarkerIcon.jsx
+++ src/components/MarkerIcon/MarkerIcon.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
-import { Icon } from 'leaflet';
-
-import PlayIcon from '../../assets/icons/play.png';
-
-const icon = new Icon({
-  iconUrl: PlayIcon,
-  iconAnchor: [17, 45],
-  popupAnchor: [3, -45],
-});
+import leafletIcon from './LeafletIcon/LeafletIcon';
 
 const MarkerIcon = ({ episodesList }) =>
   episodesList.map((episodeItem) => (
     <Marker
       data-testid="episode-title"
       position={episodeItem.coordinates}
-      icon={icon}
+      icon={leafletIcon}
       key={episodeItem.id}
     >
       <Popup>
