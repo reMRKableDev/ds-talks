4bb26c137fd8c8e0b18fb8ce067e7b8fb6d3a098
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-10 23:21:07 +0100

    :sparkles: New player feature in progress

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactPlayer from 'react-player/lazy';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 const Player = () => (
-  <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+  <AudioPlayer src="https://www.buzzsprout.com/1078358/7235866-39-kizomba-harmony-on-the-mission-to-create-the-future-kizomba-scene.mp3" />
 );
 
 export default Player;
