980d57be91351643eff7c037f6195653206cb8c5
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-14 23:03:01 +0100

    :sparkles: Info text added
    clear

---
+++ src/components/Text/Text.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Text.css';
+
+const Text = () => {
+  return (
+    <section className="text-section">
+      <h2>Welcome to the tribe of dance lovers!</h2>
+      <p>The most epic dance stories out there.</p>
+    </section>
+  );
+};
+
+export default Text;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Hero from '../components/Hero/Hero';
+import Text from '../components/Text/Text';
 
 const HomePage = () => (
   <>
     <Hero />
+    <Text />
   </>
 );
 
