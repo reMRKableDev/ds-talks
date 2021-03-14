c1e13e3cf7aa388669c107e7137a1765be7dccb4
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-14 23:30:47 +0100

    :art: Text column added

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section className="row description-section">
+      <div className="column">
+        <p>col 1</p>
+      </div>
+      <div className="column description-text">
+        <p>
+          Dance Specific Talks is a unique podcast that gives space to
+          dancer&apos;s stories worldwide.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Hero from '../components/Hero/Hero';
 import Text from '../components/Text/Text';
+import About from '../components/About/About';
 
 const HomePage = () => (
   <>
     <Hero />
     <Text />
+    <About />
   </>
 );
 
