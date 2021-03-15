e336953ba5b9b71b6b58f4e2cd46ffd43bbae57d
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-15 01:25:06 +0100

    :art: Landing page at 80%

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,13 +4,19 @@ import './About.css';
 const About = () => {
   return (
     <section className="row description-section">
-      <div className="column">
-        <p>col 1</p>
+      <div className="column description-image">
+        <figure>
+          <img src="https://via.placeholder.com/300" alt="podcast" />
+        </figure>
       </div>
-      <div className="column description-text">
+      <div className="double-column description-text">
         <p>
-          Dance Specific Talks is a unique podcast that gives space to
-          dancer&apos;s stories worldwide.
+          Dance Specific Talks is a unique podcast, where dancers can share
+          their stories.
+        </p>
+        <p>
+          Hear their incredible journeys, learn from their experiences, and much
+          more.
         </p>
       </div>
     </section>
