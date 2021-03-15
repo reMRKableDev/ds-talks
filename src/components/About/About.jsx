438809c67207ce5dad5efe9ee826e6f7a7d3a408
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-15 21:00:37 +0100

    :lipstick: temp fix on link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,15 +3,12 @@ import { Link } from '@reach/router';
 import './About.css';
 
 import MicrophoneIcon from '../../assets/icons/podcast-mic.svg';
-import MapBg from '../../assets/images/map.png';
+// import MapBg from '../../assets/images/map.png';
 
 const About = () => {
   return (
     <section className="row description-section">
-      <div
-        className="column description-image"
-        style={{ backgroundImage: `url(${MapBg})` }}
-      >
+      <div className="column description-image">
         <figure>
           <img src={MicrophoneIcon} alt="podcast-mic" />
         </figure>
@@ -22,10 +19,6 @@ const About = () => {
             Dance Specific <span>Talks</span>
           </mark>{' '}
           is a unique podcast, with stories from dancers worldwide.
-        </p>
-        <p>
-          Therese Sahlin interviews everyone from underground Hip-hop battlers
-          to famous Bachata stars.
         </p>
         <Link to="/map">See Map</Link>
       </div>
