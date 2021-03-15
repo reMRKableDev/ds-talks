354bf381101c62cacf34090eb671181b5ef2dcfc
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-15 14:19:48 +0100

    :lipstick: Added new styles for about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import './About.css';
 
+import MicrophoneIcon from '../../assets/icons/podcast-mic.svg';
+
 const About = () => {
   return (
     <section className="row description-section">
       <div className="column description-image">
         <figure>
-          <img src="https://via.placeholder.com/300" alt="podcast" />
+          <img src={MicrophoneIcon} alt="podcast-mic" />
         </figure>
       </div>
       <div className="double-column description-text">
         <p>
-          Dance Specific Talks is a unique podcast, where dancers can share
-          their stories.
+          <mark>
+            Dance Specific <span>Talks</span>
+          </mark>{' '}
+          is a unique podcast, with stories from dancers worldwide.
         </p>
         <p>
-          Hear their incredible journeys, learn from their experiences, and much
-          more.
+          Therese Sahlin interviews everyone from underground Hip-hop battlers
+          to famous Bachata stars.
         </p>
       </div>
     </section>
