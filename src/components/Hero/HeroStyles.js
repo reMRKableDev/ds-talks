cc7fc821b96dc5d0529753949760affd9ac56d18
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-17 18:40:12 +0100

    :lipstick: Final fixes of hero image

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -2,14 +2,22 @@ import styled from 'styled-components';
 
 export const StyledHeroSection = styled.section`
   margin-bottom: 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: linear-gradient(
+    145deg,
+    rgba(253, 38, 71, 1) 0%,
+    rgba(252, 128, 45, 1) 75%,
+    rgba(250, 167, 43, 1) 100%
+  );
 `;
 
 export const StyledFigure = styled.figure`
   width: 100vw;
-  height: 100vh;
 `;
 
 export const StyledImage = styled.img`
   width: 100%;
-  height: inherit;
 `;
