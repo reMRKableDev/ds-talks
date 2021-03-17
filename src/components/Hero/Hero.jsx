17f697f6150d06ba121ae3ee64331ffc47b16431
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-03-17 18:18:27 +0100

    :lipstick: Transformed .css attributes to styled-comps

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import './Hero.css';
+
+import { StyledHeroSection, StyledFigure, StyledImage } from './HeroStyles';
 
 import HeroBanner from '../../assets/images/ds-talks-banner.jpg';
 
 const Hero = () => {
   return (
-    <section className="hero-section">
-      <figure className="hero-container">
-        <img src={HeroBanner} alt="hero" className="hero-image" />
-      </figure>
-    </section>
+    <StyledHeroSection>
+      <StyledFigure>
+        <StyledImage src={HeroBanner} alt="hero" className="hero-image" />
+      </StyledFigure>
+    </StyledHeroSection>
   );
 };
 
---
+++ src/components/Hero/HeroStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledHeroSection = styled.section`
+  margin-bottom: 1em;
+`;
+
+export const StyledFigure = styled.figure`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: inherit;
+`;
