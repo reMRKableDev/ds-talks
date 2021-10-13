d844fb9387a2a78703facf5b9b3eb606db4ae2ee
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-10-13 17:57:53 +0200

    :wrench: Optimizing environment variables according to next js standard

--- src/services/apiDataService.js
+++ src/services/apiDataService.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 class ApiDataService {
   constructor() {
     this.service = axios.create({
-      baseURL: process.env.REACT_APP_BUZZSPROUT_API_KEY,
+      baseURL: process.env.NEXT_PUBLIC_BUZZSPROUT_API_KEY,
       headers: {
-        Authorization: `Bearer token=${process.env.REACT_APP_BUZZSPROUT_API_KEY}`,
+        Authorization: `Bearer token=${process.env.NEXT_PUBLIC_BUZZSPROUT_API_KEY}`,
       },
     });
   }
 
   async getAllEpisodes() {
     const response = await this.service.get(
-      'https://www.buzzsprout.com/api/1078358/episodes.json'
+      process.env.NEXT_PUBLIC_BUZZSPROUT_URL
     );
 
     return response.data;
