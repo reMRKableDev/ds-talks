35f61f9547b8415bdafdde68e2e97a46cd5229b2
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-10-14 13:23:37 +0200

    :art: importing dialog boxews

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,3 +1,6 @@
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { XIcon } from '@heroicons/react/outline';
 import NextLink from '../shared/nextLink';
 
 const Navbar = ({ handleToggleMenu }) => {
