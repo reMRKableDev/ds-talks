01a56aaedc026045e3201ce77fc9aa7d28b4c1e5
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-10-14 11:10:01 +0200

    :fire: took away code for the moment

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -7,10 +7,6 @@ const Header = () => {
       <NextLink linkTo="/map" value="Map" />
       <div>
         <p>Follow on Social Media</p>
-        <div>
-          <NextLink linkTo="/" value="Home" />
-          <NextLink linkTo="/map" value="Map" />
-        </div>
       </div>
     </nav>
   );
