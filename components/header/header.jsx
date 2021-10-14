c45f5e50ac1269329d2abcf4d2e3e618f51e60ca
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-10-14 20:08:13 +0200

    :boom: will fix these later

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,8 +1,13 @@
+import useDarkMode from '../../hooks/useDarkMode';
+
 const Header = ({ handleToggleMenu }) => {
+  // const [dark, setDark] = useDarkMode();
+
   return (
     <header>
       <div>Logo Goes here</div>
       <button onClick={handleToggleMenu}>Open</button>
+      <button>Toggle theme</button>
     </header>
   );
 };
--- hooks/useDarkMode.js
+++ hooks/useDarkMode.js
@@ -1,14 +1,47 @@
 import { useEffect, useState } from 'react';
-import { DARK_THEME, LIGHT_THEME } from '../lib/constants';
+import { DARK, LIGHT } from '../lib/constants';
 
 export default function useDarkMode() {
-  const [theme, setTheme] = useState(DARK_THEME);
-  const currentColorTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME;
+  const [dark, setDark] = useState(false);
+  console.log('🚀 ~ file: useDarkMode.js ~ line 6 ~ useDarkMode ~ dark', dark);
 
   useEffect(() => {
     const rootElement = window.document.documentElement;
-    rootElement.classList.add(theme);
-  }, [theme]);
 
-  return [currentColorTheme, setTheme];
+    if (dark || localStorage.isDarkMode) {
+      /* console.log('here 3');
+      setDark(localStorage.isDarkMode);
+      rootElement.classList.add(DARK);
+      return; */
+      console.log('here 2');
+      rootElement.classList.add(DARK);
+      localStorage.setItem('isDarkMode', dark);
+
+      /* if (dark) {
+        rootElement.classList.add(DARK);
+        localStorage.setItem('isDarkMode', dark);
+      } else {
+        console.log('here');
+        rootElement.classList.remove(DARK);
+        localStorage.removeItem('isDarkMode');
+      } */
+    } else {
+      console.log('here 3');
+      rootElement.classList.remove(DARK);
+      localStorage.removeItem('isDarkMode');
+    }
+    /* if (!localStorage.theme) {
+      localStorage.setItem('theme', LIGHT);
+      setDark(localStorage.theme);
+    } else {
+      console.log('post refresh', localStorage.theme);
+      rootElement.classList.remove(localStorage.theme);
+      rootElement.classList.add(localStorage.theme);
+      // localStorage.setItem('theme', theme);
+
+      // setTheme(localStorage.theme);
+    } */
+  }, [dark]);
+
+  return [dark, setDark];
 }
