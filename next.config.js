ce20a283bcc4e7ecaa28f1ada9e7e4ec27466e0f
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-10-19 15:34:47 +0200

    :wrench: removed locales from next.config

--- next.config.js
+++ next.config.js
@@ -6,9 +6,5 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['pages', 'lib', 'hooks', 'components'],
   },
-  i18n: {
-    locales: ['en', 'en-US', 'en-SV'],
-    defaultLocale: 'en',
-  },
   trailingSlash: true,
 });
