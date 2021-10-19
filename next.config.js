99ad33ff1a10a26cd092adb104e8ed6cf985a328
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-10-19 12:21:25 +0200

    :wrench: setting up bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-    
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  i18n: {
+    locales: ['en', 'en-US', 'en-SV'],
+    defaultLocale: 'en',
+  },
+  trailingSlash: true,
+});
