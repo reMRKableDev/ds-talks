67cc182c62f16345371914da96b3e56181281932
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2021-10-19 11:04:28 +0200

    :lipstick: migrating styles to external file

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Switch } from '@headlessui/react';
 import SiteIcon from '../shared/siteIcon';
 import useDarkMode from '../../hooks/useDarkMode';
+import { switchContainer, switchToggleWrapper } from './headerStyles';
 import { BRAND_NAME, SUN_ICON, MOON_ICON } from '../../lib/constants';
 
 const Header = ({ handleToggleMenu }) => {
@@ -17,15 +18,13 @@ const Header = ({ handleToggleMenu }) => {
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={`${
-          enabled ? 'bg-black' : 'bg-white'
-        } relative inline-flex flex-shrink-0 items-center h-6 w-12 rounded-full cursor-pointer border-2 border-transparent focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 transition-colors ease-in-out duration-200`}
+        className={`${enabled ? 'bg-black' : 'bg-white'} ${switchContainer}`}
       >
         <span
           aria-hidden="true"
           className={`${
             enabled ? 'translate-x-6' : 'translate-x-1'
-          } inline-block w-5 h-5 transform bg-brandRed rounded-full shadow-lg transform ring-0 transition ease-in-out duration-200 p-0.5`}
+          } ${switchToggleWrapper}`}
         >
           {<SiteIcon name={enabled ? MOON_ICON : SUN_ICON} />}
         </span>
---
+++ components/header/headerStyles.js
@@ -0,0 +1,5 @@
+export const switchContainer = `
+relative inline-flex flex-shrink-0 items-center h-6 w-12 rounded-full cursor-pointer border-2 border-transparent focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 transition-colors ease-in-out duration-200
+`;
+
+export const switchToggleWrapper = `inline-block w-5 h-5 transform bg-brandRed rounded-full shadow-lg transform ring-0 transition ease-in-out duration-200 p-0.5`;
